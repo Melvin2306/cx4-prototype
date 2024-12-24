3167fa5530c5a0ad981064b552f56f3c1d08737e
Melvin2306 <melvin.rinkleff@gmx.de>
2024-12-24 14:58:51 +0100

    creation of dashboard, email & tasks pages, data and functions

---
+++ app/email/page.tsx
@@ -0,0 +1,58 @@
+import { MainNav } from "@/components/main-nav";
+import { Search } from "@/components/search";
+import TeamSwitcher from "@/components/team-switcher";
+import { UserNav } from "@/components/user-nav";
+import { cookies } from "next/headers";
+import Image from "next/image";
+import { Mail } from "../../components/mail";
+import { accounts, mails } from "./data";
+
+export default function MailPage() {
+  const layout = cookies().get("react-resizable-panels:layout:mail");
+  const collapsed = cookies().get("react-resizable-panels:collapsed");
+
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
+
+  return (
+    <>
+      <div className="md:hidden">
+        <Image
+          src="/examples/mail-dark.png"
+          width={1280}
+          height={727}
+          alt="Mail"
+          className="hidden dark:block"
+        />
+        <Image
+          src="/examples/mail-light.png"
+          width={1280}
+          height={727}
+          alt="Mail"
+          className="block dark:hidden"
+        />
+      </div>
+      <div className="hidden flex-col md:flex">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+            <TeamSwitcher />
+            <MainNav className="mx-6" />
+            <div className="ml-auto flex items-center space-x-4">
+              <Search />
+              <UserNav />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hidden flex-col md:flex">
+        <Mail
+          accounts={accounts}
+          mails={mails}
+          defaultLayout={defaultLayout}
+          defaultCollapsed={defaultCollapsed}
+          navCollapsedSize={4}
+        />
+      </div>
+    </>
+  );
+}
---
+++ app/tasks/page.tsx
@@ -0,0 +1,80 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { Metadata } from "next";
+import Image from "next/image";
+import { z } from "zod";
+import { taskSchema } from "./data/schema";
+import { UserNav } from "@/components/tasks-user-nav";
+import { DataTable } from "@/components/data-table";
+import { columns } from "@/components/columns";
+import { MainNav } from "@/components/main-nav";
+import { Search } from "@/components/search";
+import TeamSwitcher from "@/components/team-switcher";
+import { UserNav as MainUserNav } from "@/components/user-nav";
+
+export const metadata: Metadata = {
+  title: "Tasks",
+  description: "A task and issue tracker build using Tanstack Table.",
+};
+
+// Simulate a database read for tasks.
+async function getTasks() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "app/tasks/data/tasks.json")
+  );
+
+  const tasks = JSON.parse(data.toString());
+
+  return z.array(taskSchema).parse(tasks);
+}
+
+export default async function TaskPage() {
+  const tasks = await getTasks();
+
+  return (
+    <>
+      <div className="md:hidden">
+        <Image
+          src="/examples/tasks-light.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/examples/tasks-dark.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="hidden flex-col md:flex">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+            <TeamSwitcher />
+            <MainNav className="mx-6" />
+            <div className="ml-auto flex items-center space-x-4">
+              <Search />
+              <MainUserNav />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of your tasks for this month!
+            </p>
+          </div>
+          <div className="flex items-center space-x-2">
+            <UserNav />
+          </div>
+        </div>
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </>
+  );
+}
